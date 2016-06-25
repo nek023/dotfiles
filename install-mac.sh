50ae9ebf820c323aa37fdf00ff4c68b51ad20d91
questbeat <questbeat@gmail.com>
2016-06-26 01:23:44 +0900

    Update install-mac.sh

--- install-mac.sh
+++ install-mac.sh
@@ -22,7 +22,7 @@ brew bundle
 
 # Install anyenv
 git clone https://github.com/riywo/anyenv ~/.anyenv
-exec $SHELL -l
+source ~/.bashrc
 
 # Install anyenv-update
 mkdir -p $(anyenv root)/plugins
@@ -40,3 +40,21 @@ pyenv global 2.7.11
 
 # Install vim plugins
 vim +PlugInstall +qa
+
+# Install apps
+mas install 950145466  # Letterspace
+mas install 409789998  # Twitter
+mas install 449589707  # Dash
+mas install 896624060  # Kobito
+mas install 406056744  # Evernote
+mas install 414030210  # LimeChat
+mas install 1024640650 # CotEditor
+mas install 425424353  # The Unarchiver
+mas install 955848755  # Theine
+mas install 682658836  # GarageBand
+mas install 409203825  # Numbers
+mas install 409201541  # Pages
+mas install 409183694  # Keynote
+mas install 408981434  # iMovie
+mas install 497799835  # Xcode
+
