2b12f515661d00fbfc20f7789e6126c2531166e5
questbeat <questbeat@gmail.com>
2016-08-25 12:18:05 +0900

    Fix knife_solo_provision_target

--- dotfiles/.config/fish/functions/knife_solo_provision_target.fish
+++ dotfiles/.config/fish/functions/knife_solo_provision_target.fish
@@ -1,10 +1,5 @@
 function knife_solo_provision_target
   select_target_host | read -l target_host; \
-    and vagrant destroy -f; \
-    and vagrant up; \
-    and vagrant snapshot take init; \
     and knife solo prepare $target_host; \
-    and vagrant snapshot take prepared; \
-    and knife solo cook $target_host; \
-    and vagrant snapshot take cooked
+    and knife solo cook $target_host
 end
