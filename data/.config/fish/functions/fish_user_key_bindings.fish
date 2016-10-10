47451391ca954dbaa3cf542e1f4502f4a82bb4b9
questbeat <questbeat@gmail.com>
2016-10-10 15:41:22 +0900

    Add fish plugins

--- data/.config/fish/functions/fish_user_key_bindings.fish
+++ data/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,12 +1,6 @@
 function fish_user_key_bindings
   bind \cg\ca __select_git_add
   bind \cga   __select_git_add
-	bind \cgc   __select_ghq_cd
-	bind \cr    __select_history
-
-  bind \cg\cg\cb __git_all_recent_branches
-  bind \cg\cb    __git_recent_branches
-  bind \cgb      __git_recent_branches
 
   bind \cj __expand_global_alias
   bind \r  __expand_global_alias
--- data/.config/fish/plugins.fish
+++ data/.config/fish/plugins.fish
@@ -1,5 +1,9 @@
 oh-my-fish/plugin-cd
 oh-my-fish/plugin-gi
 oh-my-fish/plugin-osx
-masa0x80/complement_ssh_host.fish
+masa0x80/angler.fish
 masa0x80/complement_mkr_host_ip.fish
+masa0x80/complement_ssh_host.fish
+masa0x80/filtering_history_keybind.fish
+masa0x80/ghq_cd_keybind.fish
+masa0x80/git_checkout_keybind.fish
