e9633965c6e66955468a2113b8c54b1f9bc38983
questbeat <questbeat@gmail.com>
2017-01-10 23:03:11 +0900

    Update key bindings

--- data/.config/fish/functions/fish_user_key_bindings.fish
+++ data/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,6 @@
 function fish_user_key_bindings
-  bind \cg\ca __select_git_add
-  bind \cga   __select_git_add
+  bind \c{ backward-word
+  bind \c} forward-word
 
   bind \cj __expand_global_alias
   bind \r  __expand_global_alias
--- data/.config/fish/plugins.fish
+++ data/.config/fish/plugins.fish
@@ -6,4 +6,5 @@ masa0x80/complement_mkr_host_ip.fish
 masa0x80/complement_ssh_host.fish
 masa0x80/filtering_history_keybind.fish
 masa0x80/ghq_cd_keybind.fish
+masa0x80/git_add_keybind.fish
 masa0x80/git_checkout_keybind.fish
