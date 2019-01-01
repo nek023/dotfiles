a13e16ab24ab8c9045fd33570446eb1851430710
questbeat <questbeat@gmail.com>
2019-01-01 17:32:20 +0900

    Update install script

--- README.md
+++ README.md
@@ -1,28 +1,7 @@
 # dotfiles
 
-## Install on local
-
-Make sure that `git` and `ruby` is installed.
-
 ```
-$ git clone git@github.com:questbeat/dotfiles.git
-$ cd dotfiles
-$ make
-```
-
-
-## Install on remote
-
-```
-$ git clone git@github.com:questbeat/dotfiles.git
-$ cd dotfiles
-$ gem install bundler
-$ bundle install --path=vendor/bundle
-$ bundle exec itamae ssh --host HOST roles/linux/default.rb -y nodes/linux.yml
-```
-
-If you deploy to vagrant, use `--vagrant` option.
-
-```
-$ bundle exec itamae ssh --vagrant --host HOST roles/linux/default.rb -y nodes/linux.yml
+$ git clone git@github.com:questbeat/dotfiles.git $HOME/dotfiles
+$ cd $HOME/dotfiles
+$ make link
 ```
--- install.sh
+++ install.sh
@@ -1,13 +1,46 @@
 #!/usr/bin/env bash
 
-# Detect role
-ROLE=$(echo $(uname) | tr "[:upper:]" "[:lower:]")
-NODE=nodes/${ROLE}.yml
-RECIPE=roles/${ROLE}/default.rb
+if [ "$(uname)" == 'Darwin' ]; then
+  # Install Xcode command line tools
+  xcode-select --install
+  while :
+  do
+    xcode-select -p
+    if [ $? -eq 0 ]; then
+      break
+    fi
+    sleep 5
+  done
+fi
 
-# Install gems
-gem install bundler
-bundle install --path=vendor/bundle
+# Install dotfiles
+git clone https://github.com/questbeat/dotfiles.git $HOME/dotfiles
+pushd $HOME/dotfiles > /dev/null
+make link
+popd > /dev/null
 
-# Run provisioning
-bundle exec itamae local ${RECIPE} -y ${NODE}
+if [ "$(uname)" == 'Darwin' ]; then
+  # Install homebrew
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+  # Install homebrew/bundle
+  brew tap homebrew/bundle
+
+  # Install from Brewfile
+  brew bundle --file=$HOME/dotfiles/Brewfile
+fi
+
+# Install anyenv
+git clone https://github.com/riywo/anyenv $HOME/.anyenv
+source $HOME/.bashrc
+
+# Install anyenv-update
+mkdir -p $(anyenv root)/plugins
+git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
+
+# Install envs
+anyenv install rbenv
+anyenv install pyenv
+
+# Install vim plugins
+vim +PlugInstall +qa
