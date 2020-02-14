abf5f570a9574502a04627b3d80fe876ca7e7de7
questbeat <questbeat@gmail.com>
2020-02-14 23:40:05 +0900

    __workdirを修正

--- .config/fish/functions/__workdir.fish
+++ .config/fish/functions/__workdir.fish
@@ -1,5 +1,7 @@
 function __workdir
-    if test (git rev-parse --is-inside-work-tree 2>/dev/null) = 'true'
+    set -l inside_work_tree (git rev-parse --is-inside-work-tree 2>/dev/null)
+
+    if test "$inside_work_tree" = 'true'
         echo (__short_dirname (git rev-parse --show-toplevel))
     else
         echo (__short_dirname (pwd))
