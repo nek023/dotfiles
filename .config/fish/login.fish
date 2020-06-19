1d418c127a8f4118b1f79ec98c0d02559dc4ec0c
questbeat <questbeat@gmail.com>
2020-06-19 23:59:12 +0900

    fish_user_pathsではなくPATHに追加する

--- .config/fish/login.fish
+++ .config/fish/login.fish
@@ -6,12 +6,7 @@
 # ------------------------------------------------------------------------------
 # /usr/local/binのあとに/usr/local/sbinを追加する
 # fish_user_pathsではなくPATHに追加しないと、anyenv init実行時に順番がおかしくなる
-set -gx PATH /usr/local/bin /usr/local/sbin (string match -v /usr/local/bin $PATH)
-
-# Google Cloud SDK
-if test -f /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
-    source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
-end
+set -x PATH /usr/local/bin /usr/local/sbin (string match -v /usr/local/bin $PATH)
 
 # anyenv
 if test -d $HOME/.anyenv
@@ -20,21 +15,21 @@ end
 
 # go
 if test -d $GOPATH/bin
-    set -gx fish_user_paths $GOPATH/bin $fish_user_paths
-end
-
-# heroku
-if test -d /usr/local/heroku/bin
-    set -gx fish_user_paths /usr/local/heroku/bin $fish_user_paths
+    set -x PATH $GOPATH/bin $PATH
 end
 
 # rust
 if test -d $HOME/.cargo/bin
-    set -gx fish_user_paths $HOME/.cargo/bin $fish_user_paths
+    set -x PATH $HOME/.cargo/bin $PATH
+end
+
+# Google Cloud SDK
+if test -f /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+    source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
 end
 
 # dotfiles/bin
-set -gx fish_user_paths $HOME/dotfiles/bin $fish_user_paths
+set -x PATH $HOME/dotfiles/bin $PATH
 
 # ------------------------------------------------------------------------------
 # startup
