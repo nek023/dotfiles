92630869bccfb262acc509137f47df344c71d302
questbeat <questbeat@gmail.com>
2020-06-19 23:11:30 +0900

    リファクタリング

--- .config/fish/functions/__tmux_create_session.fish
+++ .config/fish/functions/__tmux_create_session.fish
@@ -1,5 +1,5 @@
 function __tmux_create_session
-    if begin; type -aq tmux && not __tmux_is_running && not __ssh_is_running; end
+    if type -aq tmux && not __tmux_is_running && not __ssh_is_running
         tmux new -s (__tmux_nice_session_name)
     end
 end
