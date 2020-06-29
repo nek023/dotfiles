16586c18c9694c650943f2df46004e4b8fe481e0
questbeat <questbeat@gmail.com>
2020-06-29 23:25:42 +0900

    __select_historyを修正

--- .config/fish/functions/__select_history.fish
+++ .config/fish/functions/__select_history.fish
@@ -1,6 +1,6 @@
 function __select_history
   set -l buffer (commandline)
   set -l cmd (history | fzf +m +s)
-  test -n "$command" && commandline "$command"
+  test -n "$cmd" && commandline "$cmd"
   commandline -f repaint
 end
