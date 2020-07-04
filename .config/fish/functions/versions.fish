3d6066340ba6f9b5532e4070ce41a4aeb28e2d70
questbeat <questbeat@gmail.com>
2020-07-04 15:56:05 +0900

    versionsを追加

--- .config/fish/functions/versions.fish
+++ .config/fish/functions/versions.fish
@@ -1,6 +1,22 @@
-function versions -a lang
-    set -l files (find (ghq root) -name ".$lang-version" -type f -maxdepth 4)
-    if test (count $files) -gt 0
-        cat $files | sort | uniq
+function versions
+    argparse -n versions 'h/help' 'i/installed' 'n/not-installed' 'u/used' -- $argv || return
+
+    if set -q _flag_help
+        echo 'usage: versions [--installed] [--not-installed] [--used] <lang>'
+    else if set -q _flag_installed
+        if test -n "$argv"
+            asdf list "$argv" | string trim
+        else
+            asdf list
+        end
+    else if set -q _flag_not_installed
+        sort (versions -u $argv | psub) (versions -i $argv | psub) (versions -i $argv | psub) | \
+            uniq -u
+    else if set -q _flag_used
+        set -l files (find (ghq root) -name ".$argv-version" -type f -maxdepth 4)
+        test (count $files) -eq 0 && return
+        cat $files | sort -u
+    else
+        versions -h
     end
 end
