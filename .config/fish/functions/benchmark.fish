980054b728c4851e5375f020bec1d4a98efb5f58
questbeat <questbeat@gmail.com>
2020-07-06 14:43:33 +0900

    benchmarkコマンドを更新

--- .config/fish/functions/benchmark.fish
+++ .config/fish/functions/benchmark.fish
@@ -4,16 +4,23 @@ function benchmark
     set -l cnt $_flag_count
     test -z "$cnt" && set cnt 1
 
-    if test -z "$argv"
+    set -l cmd $argv
+    if test -z "$cmd"
         echo 'usage: benchmark [--count=<num>] <command>'
         return
     end
 
-    set -l sum 0
+    set -l sum 0 0 0
     for i in (seq 1 $cnt)
-        set res (gtime -f "%e" $argv 2>&1 | tail -n 1)
-        set sum (math $sum + (gtime -f "%e" $argv 2>&1 > /dev/null | tail -n 1))
+        set -l res (gtime -f "%U\t%S\t%e" $cmd 2>&1 > /dev/null | tail -n 1)
+        for j in (seq 1 3)
+            set sum[$j] (math $sum[$j] + (echo $res | cut -f $j))
+        end
     end
 
-    echo (math $sum \* 1000 / $cnt) ms
+    echo "cmd: $cmd"
+    echo "cnt: $cnt"
+    echo "usr:" (math $sum[1] \* 1000 / $cnt) ms
+    echo "sys:" (math $sum[2] \* 1000 / $cnt) ms
+    echo "rea:" (math $sum[3] \* 1000 / $cnt) ms
 end
