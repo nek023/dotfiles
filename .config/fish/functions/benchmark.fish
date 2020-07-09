3283701971df1f97d7b4c27769b7e5b13c012852
questbeat <questbeat@gmail.com>
2020-07-10 00:05:46 +0900

    benchmarkコマンドを更新

--- .config/fish/functions/benchmark.fish
+++ .config/fish/functions/benchmark.fish
@@ -12,15 +12,13 @@ function benchmark
 
     set -l sum 0 0 0
     for i in (seq 1 $cnt)
-        set -l res (gtime -f "%U\t%S\t%e" $cmd 2>&1 > /dev/null | tail -n 1)
+        set -l res (gtime -f "%e\t%U\t%S" $cmd 2>&1 > /dev/null | tail -n 1)
         for j in (seq 1 3)
             set sum[$j] (math $sum[$j] + (echo $res | cut -f $j))
         end
     end
 
-    echo "cmd: $cmd"
-    echo "cnt: $cnt"
-    echo "usr:" (math $sum[1] \* 1000 / $cnt) ms
-    echo "sys:" (math $sum[2] \* 1000 / $cnt) ms
-    echo "rea:" (math $sum[3] \* 1000 / $cnt) ms
+    echo "rea:" (math $sum[1] \* 1000 / $cnt) ms
+    echo "usr:" (math $sum[2] \* 1000 / $cnt) ms
+    echo "sys:" (math $sum[3] \* 1000 / $cnt) ms
 end
