ac01c90b7082c777c34be730be6ac2fb26f369a0
questbeat <questbeat@gmail.com>
2020-07-30 01:14:03 +0900

    versionsコマンドを改良

--- .config/fish/functions/versions.fish
+++ .config/fish/functions/versions.fish
@@ -1,22 +1,26 @@
-function versions
-    argparse -n versions 'h/help' 'i/installed' 'n/not-installed' 'u/used' -- $argv || return
-
-    if set -q _flag_help
-        echo 'usage: versions [--installed] [--not-installed] [--used] <lang>'
-    else if set -q _flag_installed
-        if test -n "$argv"
-            asdf list "$argv" | string trim
+function versions -a cmd lang
+    switch $cmd
+    case installed
+        if test -n "$lang"
+            asdf list "$lang" | string trim
         else
             asdf list
         end
-    else if set -q _flag_not_installed
-        sort (versions -u $argv | psub) (versions -i $argv | psub) (versions -i $argv | psub) | \
-            uniq -u
-    else if set -q _flag_used
-        set -l files (find (ghq root) -name ".$argv-version" -type f -maxdepth 4)
-        test (count $files) -eq 0 && return
-        cat $files | sort -u
-    else
-        versions -h
+
+    case used
+        if test -z "$lang"
+            echo 'Usage: versions used LANG'
+            return 1
+        end
+
+        set -l files (find (ghq root) -name ".$lang-version" -type f -maxdepth 4)
+        paste (cat $files | psub) (string replace -r '^(.*)$' '($1)' $files | psub) | sort
+
+    case '*'
+        echo 'Usage: versions <command> [args]'
+        echo ''
+        echo 'Commands:'
+        echo '    installed          List installed versions'
+        echo '    used               List used versions'
     end
 end
