c3f63ca6ec67e473c844a1cbe9d1efbbaa60ca35
questbeat <questbeat@gmail.com>
2021-08-01 23:13:25 +0900

    __git_select_commitを修正

--- .config/fish/functions/__git_select_commit.fish
+++ .config/fish/functions/__git_select_commit.fish
@@ -1,6 +1,6 @@
 # https://github.com/junegunn/fzf/wiki/Examples#git
 function __git_select_commit
-    argparse -n __git_select_branch 'q/query=' -- $argv || return
+    argparse -n __git_select_commit 'q/query=' -- $argv || return
     git log --graph --color --format='%C(auto)%h%d %s %C(black)%C(bold)%cr' $argv | \
         fzf --ansi +m +s -q "$_flag_query" | grep -o '[a-f0-9]\{7\}' | head -1
 end
