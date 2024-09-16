78027d817989aa09fcae177eac55d709a41f1097
Katsuma Tanaka <questbeat@gmail.com>
2024-09-16 19:35:10 +0900

    Fix keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,12 +1,12 @@
-vim.keymap.set("n", "H", "b")
-vim.keymap.set("n", "J", "}")
-vim.keymap.set("n", "K", "{")
-vim.keymap.set("n", "L", "w")
+vim.keymap.set("", "H", "^")
+vim.keymap.set("", "J", "}")
+vim.keymap.set("", "K", "{")
+vim.keymap.set("", "L", "$")
 
-vim.keymap.set("n", "j", "gj")
-vim.keymap.set("n", "k", "gk")
+vim.keymap.set("", "j", "gj")
+vim.keymap.set("", "k", "gk")
 
-vim.keymap.set("n", "9", "^")
-vim.keymap.set("n", "0", "$")
+vim.keymap.set("", "9", "^")
+vim.keymap.set("", "0", "$")
 
 vim.keymap.set("n", "<C-e>", ":NvimTreeToggle<CR>")
