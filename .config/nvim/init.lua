8f067aa1d2dd044b4495185853e32826b2c1f3c9
Katsuma Tanaka <questbeat@gmail.com>
2024-09-18 14:59:05 +0900

    Update keymaps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,3 @@
+require("config.lazy")
 require("options")
 require("keymaps")
-require("config.lazy")
--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,3 +1,5 @@
+vim.g.mapleader = "\\"
+
 vim.keymap.set("", "H", "^")
 vim.keymap.set("", "J", "}")
 vim.keymap.set("", "K", "{")
@@ -9,4 +11,12 @@ vim.keymap.set("", "k", "gk")
 vim.keymap.set("", "9", "^")
 vim.keymap.set("", "0", "$")
 
+-- nvim-telescope
+local telescope = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", telescope.find_files)
+vim.keymap.set("n", "<leader>fg", telescope.live_grep)
+vim.keymap.set("n", "<leader>fb", telescope.buffers)
+vim.keymap.set("n", "<leader>fh", telescope.help_tags)
+
+-- nvim-tree
 vim.keymap.set("n", "<C-e>", ":NvimTreeToggle<CR>")
