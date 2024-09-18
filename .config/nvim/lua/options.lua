a230b38e7a01966a17652be382fd13e5848a3e23
Katsuma Tanaka <questbeat@gmail.com>
2024-09-18 15:17:17 +0900

    Update nvim options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,15 +1,24 @@
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
 vim.opt.number = true
 
 vim.opt.autoindent = true
-vim.opt.autoread = true
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
 vim.opt.smartindent = true
+
+vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.tabstop = 4
 
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+vim.opt.incsearch = true
+
+vim.opt.autoread = true
+vim.opt.backup = false
+vim.opt.swapfile = false
 
 vim.opt.clipboard:append({"unnamedplus"})
 vim.opt.termguicolors = true
