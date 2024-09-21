94bf94b3628c89eca763c305e186d0713ad52bbd
Katsuma Tanaka <questbeat@gmail.com>
2024-09-21 16:10:39 +0900

    Update neovim config

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,13 +1,50 @@
+-- 高速移動
 vim.keymap.set("", "H", "^")
 vim.keymap.set("", "J", "}")
 vim.keymap.set("", "K", "{")
 vim.keymap.set("", "L", "$")
 
+-- 行移動を見た目上の移動にする
 vim.keymap.set("", "j", "gj")
 vim.keymap.set("", "k", "gk")
 
+-- 行頭・行末移動
 vim.keymap.set("", "9", "^")
 vim.keymap.set("", "0", "$")
+
+-- 挿入モードでShift+Tabでインデントを減らす
+vim.keymap.set("i", "<S-Tab>", "<C-d>")
+
+-- ビジュアルモードでインデントを継続的に変更できるようにする
+vim.keymap.set("v", ">", ">gv")
+vim.keymap.set("v", "<", "<gv")
+
+-- 検索にヒットした候補に移動したときに表示位置を中央に修正する
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
+vim.keymap.set("n", "*", "*zz")
+vim.keymap.set("n", "#", "#zz")
+vim.keymap.set("n", "g*", "g*zz")
+vim.keymap.set("n", "g#", "g#zz")
+
+-- 検索にヒットした文字列のハイライトを非表示にする
+vim.keymap.set("n", "<Esc><Esc>", ":nohlsearch<CR>", { silent = true })
+
+-- ウィンドウのリサイズ幅を変更する
+vim.keymap.set("n", "<C-w>+", ":resize +5<CR>", { silent = true })
+vim.keymap.set("n", "<C-w>-", ":resize -5<CR>", { silent = true })
+vim.keymap.set("n", "<C-w>>", ":vertical resize +10<CR>", { silent = true })
+vim.keymap.set("n", "<C-w><", ":vertical resize -10<CR>", { silent = true })
+
+-- 新しいタブを開く
+vim.keymap.set("n", "tt", ":tablast <bar> tabnew<CR>", { silent = true })
+
+-- タブを閉じる
+vim.keymap.set("n", "tc", ":tabclose<CR>", { silent = true })
+
+-- 前後のタブに移動する
+vim.keymap.set("n", "tn", ":tabnext<CR>", { silent = true })
+vim.keymap.set("n", "tp", ":tabprevious<CR>", { silent = true })
 
 -- nvim-telescope
 local telescope = require("telescope.builtin")
@@ -17,4 +54,4 @@ vim.keymap.set("n", "<leader>fb", telescope.buffers)
 vim.keymap.set("n", "<leader>fh", telescope.help_tags)
 
 -- nvim-tree
-vim.keymap.set("n", "<C-e>", ":NvimTreeToggle<CR>")
+vim.keymap.set("n", "<C-e>", ":NvimTreeToggle<CR>", { silent = true })
--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,24 +1,41 @@
-vim.scriptencoding = "utf-8"
-vim.opt.encoding = "utf-8"
-vim.opt.fileencoding = "utf-8"
+-- https://neovim.io/doc/user/options.html
+-- https://neovim.io/doc/user/vim_diff.html
 
+-- 行番号を表示
 vim.opt.number = true
 
-vim.opt.autoindent = true
-vim.opt.expandtab = true
+-- 自動インデントでコードの構造に応じたインデントを挿入する
 vim.opt.smartindent = true
 
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
+-- タブ文字の代わりにスペースを挿入する
+vim.opt.expandtab = true
+
+-- タブ文字を何文字のスペースとして表示するか
 vim.opt.tabstop = 4
 
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.incsearch = true
+-- タブ文字をキーボードで入力した際に何文字の空白文字にするか
+vim.opt.softtabstop = 4
 
-vim.opt.autoread = true
-vim.opt.backup = false
+-- インデントを変更する際に何文字分ずらすか
+vim.opt.shiftwidth = 4
+
+-- インデント幅がshiftwidthの値の倍数になるようにスペースを挿入する
+vim.opt.shiftround = true
+
+-- 複数のコマンドがマッチする場合、リストを表示して最初の候補を挿入する
+vim.opt.wildmode = "list:full"
+
+-- 検索時に大文字小文字を無視する
+vim.opt.ignorecase = true
+
+-- 大文字を含めて検索した場合は大文字小文字を無視しない
+vim.opt.smartcase = true
+
+-- バッファのスワップファイルを作成しない
 vim.opt.swapfile = false
 
+-- システムのクリップボードと連携する
 vim.opt.clipboard:append({"unnamedplus"})
+
+--  24bit colorを有効化
 vim.opt.termguicolors = true
