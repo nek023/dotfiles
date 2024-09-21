5763cb2c97500f999807cc57cbad9e95d9dc61ea
Katsuma Tanaka <questbeat@gmail.com>
2024-09-21 16:27:39 +0900

    Format config

--- .config/nvim/lua/plugins/tree.lua
+++ .config/nvim/lua/plugins/tree.lua
@@ -1,23 +1,29 @@
 -- https://github.com/nvim-tree/nvim-tree.lua?tab=readme-ov-file#custom-mappings
 local function tree_on_attach(bufnr)
-  local api = require("nvim-tree.api")
+    local api = require("nvim-tree.api")
 
-  local function opts(desc)
-    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-  end
+    local function opts(desc)
+        return {
+            desc = "nvim-tree: " .. desc,
+            buffer = bufnr,
+            noremap = true,
+            silent = true,
+            nowait = true
+        }
+    end
 
-  -- default mappings
-  api.config.mappings.default_on_attach(bufnr)
+    -- default mappings
+    api.config.mappings.default_on_attach(bufnr)
 
-  -- custom mappings
-  vim.keymap.set("n", "<C-e>", api.tree.close, opts("Close Tree"))
+    -- custom mappings
+    vim.keymap.set("n", "<C-e>", api.tree.close, opts("Close Tree"))
 end
 
 return {{
     "nvim-tree/nvim-tree.lua",
     config = function()
         require("nvim-tree").setup({
-          on_attach = tree_on_attach,
+            on_attach = tree_on_attach
         })
     end
 }}
