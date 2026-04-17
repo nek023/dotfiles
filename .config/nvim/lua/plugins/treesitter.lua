5f34d1b7b8662ab89a44b91575dbdc2288373502
Katsuma Tanaka <1830355+nek023@users.noreply.github.com>
2026-04-17 12:47:27 +0900

    Migrate nvim-treesitter to main branch for Neovim 0.12

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,19 +1,22 @@
 return {{
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
     config = function()
-        local configs = require("nvim-treesitter.configs")
+        require("nvim-treesitter").setup({
+            install_dir = vim.fn.stdpath("data") .. "/site"
+        })
+        require("nvim-treesitter").install({"bash", "go", "javascript", "json", "json5", "lua", "make", "markdown",
+                                            "ruby", "terraform", "typescript"})
 
-        configs.setup({
-            ensure_installed = {"bash", "go", "javascript", "json", "json5", "lua", "make", "markdown", "ruby",
-                                "terraform", "typescript"},
-            sync_install = false,
-            highlight = {
-                enable = true
-            },
-            indent = {
-                enable = true
-            }
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = {"bash", "go", "javascript", "json", "json5", "lua", "make", "markdown", "ruby", "terraform",
+                       "typescript"},
+            callback = function()
+                vim.treesitter.start()
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end
         })
     end
 }}
