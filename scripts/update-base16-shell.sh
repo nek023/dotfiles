05560a4d319d0cb83f7fb735c13fd57a0a8ddc98
Katsuma Tanaka <1830355+nek023@users.noreply.github.com>
2026-05-13 22:28:24 +0900

    Re-clone base16-shell when local repo is invalid

--- scripts/update-base16-shell.sh
+++ scripts/update-base16-shell.sh
@@ -3,10 +3,14 @@
 set -eu
 
 BASE16_DIR="${XDG_CONFIG_HOME}/base16-shell"
+BASE16_REPO="https://github.com/chriskempson/base16-shell.git"
 
-if [ -d $BASE16_DIR ]; then
-  cd $BASE16_DIR
-  git pull origin master
+if git -C "$BASE16_DIR" rev-parse --git-dir >/dev/null 2>&1; then
+  git -C "$BASE16_DIR" pull origin master
 else
-  git clone https://github.com/chriskempson/base16-shell.git $BASE16_DIR
+  if [ -e "$BASE16_DIR" ]; then
+    echo "Removing invalid base16-shell directory: $BASE16_DIR"
+    rm -rf "$BASE16_DIR"
+  fi
+  git clone "$BASE16_REPO" "$BASE16_DIR"
 fi
