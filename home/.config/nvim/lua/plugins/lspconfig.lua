ce3992086941298d76e5a36043d4003f55033a62
Katsuma Tanaka <1830355+nek023@users.noreply.github.com>
2026-06-12 23:40:36 +0900

    Add nvim-lspconfig so gopls and ruby_lsp actually start

---
+++ home/.config/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,5 @@
+-- nvim-lspconfig ships the lsp/<name>.lua definitions (cmd / filetypes /
+-- root_markers) that vim.lsp.enable() in lua/lsp.lua looks up by server name.
+return {{
+    "neovim/nvim-lspconfig"
+}}
