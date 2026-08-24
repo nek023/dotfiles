424d4815144f69980eaa513876913b83d384ba5e
Katsuma Tanaka <1830355+nek023@users.noreply.github.com>
2026-08-24 21:16:24 +0900

    Pin vim plugins with a vim-plug snapshot

--- scripts/update-vim-plugins.sh
+++ scripts/update-vim-plugins.sh
@@ -2,10 +2,19 @@
 
 set -eu
 
+SNAPSHOT="${HOME}/.vim/plug-snapshot.vim"
+
 if [ ! -f ~/.vim/autoload/plug.vim ]; then
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 if command -v vim >/dev/null 2>&1; then
-  vim +PlugUpgrade +PlugUpdate +qa
+  vim +PlugUpgrade +PlugUpdate +"PlugSnapshot! ${SNAPSHOT}" +qa
+
+  # PlugSnapshot は 2 行目に生成時刻を書くので、更新が無くても差分になる。落とす。
+  # stow の symlink を壊さないよう mv ではなくリダイレクトで上書きする。
+  tmp="$(mktemp)"
+  sed '2d' "${SNAPSHOT}" > "${tmp}"
+  cat "${tmp}" > "${SNAPSHOT}"
+  rm -f "${tmp}"
 fi
