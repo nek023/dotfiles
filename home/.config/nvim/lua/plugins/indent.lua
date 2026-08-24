3f4bfedb222535e19207835c0b90cde350d5492a
Katsuma Tanaka <1830355+nek023@users.noreply.github.com>
2026-08-24 18:50:09 +0900

    Add indent-blankline for Neovim indent guides

---
+++ home/.config/nvim/lua/plugins/indent.lua
@@ -0,0 +1,10 @@
+return {{
+    "lukas-reineke/indent-blankline.nvim",
+    version = "*",
+    main = "ibl",
+    opts = {
+        indent = {
+            smart_indent_cap = false
+        }
+    }
+}}
